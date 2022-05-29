3c125b5e86d8b571c69e6ca0d8c5e0b24ee2a0ba
ingimar-eyfjord <ingimareys93@gmail.com>
2022-05-29 19:52:48 +0200

    add globals again

--- apis/api-upload-item.php
+++ apis/api-upload-item.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-// require_once(__DIR__ . '/../globals.php');
+require_once(__DIR__ . '/../globals.php');
 
 // TODO validate will add for exam
 // if(!isset($_POST['submit'])){
